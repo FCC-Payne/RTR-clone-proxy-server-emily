33495bbb272cfd2d27fba73bd8eb603f3651b70d
Emily Caples <etcaples@gmail.com>
2018-06-14 22:03:52 -0700

    added node_modules to gitignore;

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use('/:id', express.static(path.join(__dirname, '/../public')));
 
 app.get('/:id/reviews', (req, res) => {
-
+// localhost/req.params.id/
 });
 
 app.listen(3000);
