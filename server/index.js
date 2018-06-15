e28caaece774620b853f1d990559f98a20fba31e
Emily Caples <etcaples@gmail.com>
2018-06-15 12:05:56 -0700

    runs RTR reviews module on proxy server & serves static files and data to page

--- server/index.js
+++ server/index.js
@@ -5,8 +5,17 @@ const app = express();
 
 app.use('/:id', express.static(path.join(__dirname, '/../public')));
 
-app.get('/:id/reviews', (req, res) => {
-// localhost/req.params.id/
+app.get('/:id/reviews/bundle', (req, res) => {
+    console.log('--------hello');
+    res.redirect(`http://localhost:3002/${req.params.id}/bundle.js`);
 });
+
+app.get('/:id/reviews/styles', (req, res) => {
+    res.redirect(`http://localhost:3002/${req.params.id}/stylesheet.css`);
+});
+
+// app.get('/:id/reviews', (req, res) => {
+//     res.redirect(`http://localhost:3002/${req.params.id}/reviews`);
+// });
 
 app.listen(3000);
