47b7246d4dc7f0df5795cae07aaf9811d91bf6eb
Emily Caples <etcaples@gmail.com>
2018-06-15 18:12:05 -0700

    renders reviews module and recommendation carousel to proxy page - needed to call for localhost in the url bar instead of 127.0.1

--- server/index.js
+++ server/index.js
@@ -5,8 +5,17 @@ const app = express();
 
 app.use('/:id', express.static(path.join(__dirname, '/../public')));
 
+/* RECS CAROUSEL */
+app.get('/:id/recs/bundle', (req, res) => {
+    res.redirect(`http://localhost:3004/${req.params.id}/bundle.js`);
+});
+
+app.get('/:id/recs/styles', (req, res) => {
+    res.redirect(`http://localhost:3004/${req.params.id}/styles.css`);
+});
+
+/* REVIEWS */
 app.get('/:id/reviews/bundle', (req, res) => {
-    console.log('--------hello');
     res.redirect(`http://localhost:3002/${req.params.id}/bundle.js`);
 });
 
