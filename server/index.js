62dc53377ffa8a8a448ed17577062fbd70a12fd6
Emily Caples <etcaples@gmail.com>
2018-06-16 10:44:31 -0700

    added styles to proxy server; refactored uneven spacing

--- server/index.js
+++ server/index.js
@@ -5,6 +5,20 @@ const app = express();
 
 app.use('/:id', express.static(path.join(__dirname, '/../public')));
 
+/* PHOTO VIEWER */
+app.get('/:id/photos/bundle', (req, res) => {
+    res.redirect(`http://localhost:3001/${req.params.id}/bundle.js`);
+});
+
+app.get('/:id/photos/styles', (req, res) => {
+    res.redirect(`http://localhost:3001/${req.params.id}/styles.css`);
+});
+
+/* PRODUCT FORMS */
+app.get('/:id/forms/bundle', (req, res) => {
+    res.redirect(`http://localhost:3003/${req.params.id}/bundle.js`);
+});
+
 /* RECS CAROUSEL */
 app.get('/:id/recs/bundle', (req, res) => {
     res.redirect(`http://localhost:3004/${req.params.id}/bundle.js`);
